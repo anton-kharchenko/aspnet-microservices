b7c0ee858abf07f3f440ac89c12109818fe0bb6b
Anton Kharchenko <anonymousstylekiller@gmail.com>
2023-01-01 22:54:47 +0200

    feat(application): add shopping cart entities
    Add necessary entity for shopping card
    
    [skip ci]
    
    Signed-off-by: Anton <anonymousStyleKiller@gmail.com>

---
+++ Services/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿namespace Basket.API.Entities;
+
+public class ShoppingCart
+{
+    public ShoppingCart()
+    {
+    }
+
+    public ShoppingCart(string userName)
+    {
+        UserName = userName;
+    }
+
+    public string UserName { get; set; }
+    public List<ShoppingCartItem> Items { get; set; } = new();
+
+    public decimal TotalPrice => Items.Sum(shoppingCartItem => shoppingCartItem.Price * shoppingCartItem.Quantity);
+}
