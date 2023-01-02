ebf777b6b9734f5083d5221b506b4e3050e29bcc
Anton Kharchenko <anonymousstylekiller@gmail.com>
2023-01-02 12:59:20 +0200

    feat(application): add redis configuration
    Signed-off-by: Anton <anonymousStyleKiller@gmail.com>

---
+++ Services/Basket.API/Extensions/ServiceCollectionsExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Basket.API.Extensions;
+
+public static class ServiceCollectionsExtensions
+{
+    public static void AddRedis(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddStackExchangeRedisCache(opt =>
+        {
+            opt.Configuration = configuration.GetValue<string>("CacheSettings:ConnectionString");
+        });
+    }
+    
+    public static void AddSwagger(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+}
--- Services/Basket.API/Program.cs
+++ Services/Basket.API/Program.cs
@@ -1,11 +1,14 @@
+using Basket.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwagger();
+builder.Services.AddRedis(builder.Configuration);
 
 var app = builder.Build();
 
