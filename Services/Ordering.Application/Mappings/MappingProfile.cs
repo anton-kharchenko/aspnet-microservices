7f246d0ab7dd41595a6bb732b365fd31169e065f
Anton Kharchenko <anonymousstylekiller@gmail.com>
2023-01-14 12:07:50 +0200

    feat(ordering api ): get orders list.
    realize cqrs pattern and get orders from db
    
    Signed-off-by: Anton <anonymousStyleKiller@gmail.com>

--- Services/Ordering.Application/Mappings/MappingProfile.cs
+++ Services/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,8 +1,13 @@
 ﻿using AutoMapper;
+using Ordering.Application.ViewModels;
+using Ordering.Domain.Entities;
 
 namespace Ordering.Application.Mappings;
 
 public class MappingProfile : Profile
 {
-    
+    public MappingProfile()
+    {
+        CreateMap<Order, OrdersViewModel>().ReverseMap();
+    }
 }
---
+++ Services/Ordering.Application/ViewModels/OrdersViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Ordering.Application.ViewModels;
+
+public class OrdersViewModel
+{
+    public int Id { get; set; }
+    public string UserName { get; set; } = default!;
+    public decimal TotalPrice { get; set; }
+
+    // BillingAddress
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string EmailAddress { get; set; } = default!;
+    public string AddressLine { get; set; } = default!;
+    public string Country { get; set; } = default!;
+    public string State { get; set; } = default!;
+    public string ZipCode { get; set; } = default!;
+
+    // Payment
+    public string CardName { get; set; } = default!;
+    public string CardNumber { get; set; } = default!;
+    public string Expiration { get; set; } = default!;
+    public string Cvv { get; set; } = default!;
+    public int PaymentMethod { get; set; }
+}
