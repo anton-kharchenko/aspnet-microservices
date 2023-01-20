76ffbc45638ff6502634d524ac669c27450b384e
Anton Kharchenko <anonymousstylekiller@gmail.com>
2023-01-20 22:47:40 +0200

    feat(ordering api ): pipeline mediator behaviour
    Signed-off-by: Anton <anonymousStyleKiller@gmail.com>

---
+++ Services/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using MediatR;
+using ValidationException = FluentValidation.ValidationException;
+
+
+namespace Ordering.Application.Behaviours;
+
+public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (!_validators.Any())
+            return await next();
+        var context = new ValidationContext<TRequest>(request);
+        var validationResult = await Task
+            .WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken))).ConfigureAwait(false);
+        var failures = validationResult.SelectMany(i => i.Errors).Where(e => e != null).ToArray();
+        if (failures.Length > 0)
+            throw new ValidationException(failures);
+        return await next();
+    }
+}
---
+++ Services/Ordering.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Application.Exceptions;
+
+public class NotFoundException : ApplicationException
+{
+    public NotFoundException(string name, object key) : base($"Entity \"{name}\" ({key}) was not found.")
+    {
+        
+    }
+}
