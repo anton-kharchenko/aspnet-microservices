44f634169912e7688986ec95b4427b6816ab5448
Anton Kharchenko <anonymousstylekiller@gmail.com>
2023-01-21 19:33:07 +0200

    feat(ordering api): order db context.
    add ef and mock data for db.
    
    Signed-off-by: Anton <anonymousStyleKiller@gmail.com>

---
+++ Services/Ordering.Infrastructure/Data/Mock/OrderContextMock.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Data.Context;
+
+namespace Ordering.Infrastructure.Data.Mock;
+
+public class OrderContextMock
+{
+    public static async Task MockDataAsync(OrderContext context, ILogger<OrderContextMock> logger)
+    {
+        if (!context.Orders!.Any())
+        {
+            context.Orders!.AddRange(GetPreconfiguredOrders());
+            await context.SaveChangesAsync().ConfigureAwait(false);
+        }
+    }
+
+    private static IEnumerable<Order> GetPreconfiguredOrders()
+    {
+        return new List<Order>
+        {
+            new()
+            {
+                UserName = "Anton", 
+                FirstName = "Anton",
+                LastName = "Kharhcenko", 
+                EmailAddress = "gumb1t97@gmail.com",
+                AddressLine = "Sofia", 
+                Country = "Bulguria", 
+                TotalPrice = 350
+            }
+        };
+    }
+}
