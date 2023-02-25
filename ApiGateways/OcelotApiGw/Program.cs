96a66c73baec80b4e40a5dbfa6cb6989d9e3737b
Anton Kharchenko <anonymousstylekiller@gmail.com>
2023-02-25 12:27:24 +0200

    feat(ocelot): add json configurations
    Signed-off-by: Anton <anonymousStyleKiller@gmail.com>

--- ApiGateways/OcelotApiGw/Program.cs
+++ ApiGateways/OcelotApiGw/Program.cs
@@ -7,8 +7,10 @@ builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 builder.Services.AddOcelot();
+builder.Host.ConfigureAppConfiguration(
+    (hostContext, config) => { config.AddJsonFile($"ocelot.{hostContext.HostingEnvironment.EnvironmentName}.json", true, true); }
+);
 
 var app = builder.Build();
-
 await app.UseOcelot().ConfigureAwait(false);
 app.Run();
